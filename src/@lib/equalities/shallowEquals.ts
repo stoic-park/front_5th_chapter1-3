f97f520ddbc292dae59d767fc8a4401f7efedb16
stoic-park <demige79@gmail.com>
2025-04-06 20:06:50 +0900

    feat: shallowEquals 구현

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,4 +1,32 @@
 // TODO: 과제 스타트!
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+  if (objA === objB) {
+    return true;
+  }
+  // 배열
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    return objA.every((value, index) => objB[index] === value);
+  }
+  // 객체
+  if (
+    typeof objA === "object" &&
+    objA !== null &&
+    typeof objB === "object" &&
+    objB !== null
+  ) {
+    const keysA = Object.keys(objA as Record<string, unknown>);
+    const keysB = Object.keys(objB as Record<string, unknown>);
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+    return keysA.every(
+      (key) =>
+        (objB as Record<string, unknown>)[key] ===
+        (objA as Record<string, unknown>)[key],
+    );
+  }
   return objA === objB;
 }
