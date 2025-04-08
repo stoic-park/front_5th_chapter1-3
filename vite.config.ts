eda32371a4a150065a163e9e151c3acbd6ad1353
stoic-park <demige79@gmail.com>
2025-04-08 23:25:44 +0900

    chore: 배포 관련 로직 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
   }),
   defineTestConfig({
+    base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter1-3/" : "/",
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          notFound: resolve(__dirname, "404.html"),
+        },
+      },
+    },
     test: {
       globals: true,
       environment: "jsdom",
